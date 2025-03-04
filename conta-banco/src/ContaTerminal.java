ac9d62ccb84a3f7227214e7b7ecb23840aafbdf8
Matheus Alves <ma88435@gmail.com>
2025-03-04 20:08:34 -0300

    Atualização

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,17 @@
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception{
-        System.out.println("Hello, World!");
+        //TO DO: conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem da conta criada
+
+        int numero = 1021
+        String agencia = 067-8
+        String 
+
     }
 }
